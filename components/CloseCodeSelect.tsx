c5279889a7069937c5b4edacfa83802eab42bed1
Trey Vigus <trey.vigus@gmail.com>
2025-11-06 17:13:28 -0500

    close socket dialog

---
+++ components/CloseCodeSelect.tsx
@@ -0,0 +1,51 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './shadcn/Select';
+import { useState } from 'react';
+
+export type CloseCode =
+  | '1000'
+  | '1001'
+  | '1002'
+  | '1003'
+  | '1007'
+  | '1008'
+  | '1009'
+  | '1010'
+  | '1011';
+
+export type CloseCodeItem = {
+  code: CloseCode;
+  label: string;
+};
+
+// TODO: make styling cooler (e.g. supporting text for the label)
+const closeCodeItems: CloseCodeItem[] = [
+  { code: '1000', label: 'Normal Closure' },
+  { code: '1001', label: 'Going Away' },
+  { code: '1002', label: 'Protocol Error' },
+  { code: '1003', label: 'Unsupported Data' },
+  { code: '1007', label: 'Invalid Frame Payload Data' },
+  { code: '1008', label: 'Policy Violation' },
+  { code: '1009', label: 'Message Too Big' },
+  { code: '1010', label: 'Mandatory Extension' },
+  { code: '1011', label: 'Internal Error' },
+];
+
+// TODO: make styling cooler (e.g. supporting text for the label of each code)
+//
+export function CloseCodeSelect() {
+  const [selectedCode, setSelectedCode] = useState<CloseCode>('1000');
+  return (
+    <Select value={selectedCode} onValueChange={(value) => setSelectedCode(value as CloseCode)}>
+      <SelectTrigger className="text-xs">
+        <SelectValue placeholder="1000"></SelectValue>
+      </SelectTrigger>
+      <SelectContent className="text-xs" side="bottom">
+        {closeCodeItems.map((item) => (
+          <SelectItem key={item.code} value={item.code}>
+            {item.code} ({item.label})
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
---
+++ components/CloseSocketForm.tsx
@@ -0,0 +1,40 @@
+import { CloseCodeSelect } from './CloseCodeSelect';
+import { Button } from './shadcn/Button';
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './shadcn/Dialog';
+
+export type CloseSocketFormResult = {
+  code: number;
+  reason: string;
+};
+
+export type CloseSocketFormProps = {
+  onSubmit: (packet: CloseSocketFormResult) => void;
+};
+
+export function CloseSocketForm({ onSubmit }: CloseSocketFormProps) {
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Close Socket</DialogTitle>
+          {/* explain that close code/close reason will be sent to client? */}
+          <DialogDescription>Add description here if needed</DialogDescription>
+        </DialogHeader>
+        <CloseCodeSelect></CloseCodeSelect>
+        <DialogFooter>
+          <Button type="submit">Close Socket</Button>
+        </DialogFooter>
+      </DialogContent>
+    </form>
+  );
+}

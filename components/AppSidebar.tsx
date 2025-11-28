7849d1c28ac7719b5b1da8329f15d0f26f1069ee
Trey Vigus <trey.vigus@gmail.com>
2025-11-28 16:31:12 -0500

    external link

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -60,7 +60,7 @@ function AppSidebarFooter() {
     <SidebarFooter>
       <SidebarMenu>
         <SidebarMenuItem>
-          <SidebarMenuButton asChild className="cursor-default">
+          <SidebarMenuButton asChild className="cursor-default hover:underline">
             <a
               href="https://socketinspector.com/docs/"
               target="_blank"
@@ -73,7 +73,7 @@ function AppSidebarFooter() {
           </SidebarMenuButton>
         </SidebarMenuItem>
         <SidebarMenuItem>
-          <SidebarMenuButton asChild className="cursor-default">
+          <SidebarMenuButton asChild className="cursor-default hover:underline">
             <a
               href="https://github.com/Socket-Inspector/Socket-Inspector"
               target="_blank"

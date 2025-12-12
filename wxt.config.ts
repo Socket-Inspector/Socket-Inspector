bf1d92bc5fd160146b7fdbb8894bc602090ff97e
Trey Vigus <trey.vigus@gmail.com>
2025-12-12 17:25:48 -0500

    Improve extension display name and description (#16)

--- wxt.config.ts
+++ wxt.config.ts
@@ -14,9 +14,10 @@ export default defineConfig({
   },
   manifest: ({ mode }) => {
     const config: UserManifest = {
-      name: 'Socket Inspector',
+      name: 'Socket Inspector - WebSocket Debugging Tools',
+      short_name: 'Socket Inspector',
       description:
-        'Simulate edge cases, reproduce bugs, and uncover security vulnerabilities in WebSocket Applications.',
+        'Debug WebSockets in Chrome DevTools. Inspect traffic, send custom messages, and simulate server disconnections.',
       host_permissions: ['*://*/*'],
       web_accessible_resources: [
         {

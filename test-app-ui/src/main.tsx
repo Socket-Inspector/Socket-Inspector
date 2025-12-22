27105da65f83e2928ffe909e335adf1aa4a82341
Trey Vigus <trey.vigus@gmail.com>
2025-12-21 20:43:33 -0500

    remove some tsc errors

--- test-app-ui/src/main.tsx
+++ test-app-ui/src/main.tsx
@@ -1,11 +1,5 @@
-/* oxlint-disable no-unused-vars */
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  <App />,
-  // </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);

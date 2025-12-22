a2db80229f032f3e3eff11eab12c049875ddf606
Trey Vigus <trey.vigus@gmail.com>
2025-12-22 15:40:10 -0500

    remove happy dom

--- packages/extension/vitest.config.ts
+++ packages/extension/vitest.config.ts
@@ -7,8 +7,5 @@ export default defineConfig({
     include: ['**/*.spec.ts', '**/*.spec.tsx'],
     exclude: [...configDefaults.exclude, '**/playwright/**'],
     environment: 'node',
-    // .spec.tsx files will use happy dom
-    // TODO: environmentMatchGlobs is deprecated
-    environmentMatchGlobs: [['**/*.spec.tsx', 'happy-dom']],
   },
 });

f4402924f121d9e2a9e04093efe7fc976bd8079f
Trey Vigus <trey.vigus@gmail.com>
2025-12-21 21:52:06 -0500

    no retriest

--- playwright-tests/playwright.config.ts
+++ playwright-tests/playwright.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: 'playwright/tests',
-  testMatch: '*.test.ts',
+  testDir: "playwright/tests",
+  testMatch: "*.test.ts",
 
   // Fail the build on CI if you accidentally left test.only in the source code.
   forbidOnly: !!process.env.CI,
 
   // Retry on CI only.
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
 
   // Don't run tests in parallel
   workers: 1,
 
   // Reporter to use
-  reporter: 'html',
+  reporter: "html",
 
   use: {
     // Collect trace when retrying the failed test.
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   timeout: 10000,
@@ -26,8 +26,8 @@ export default defineConfig({
   // Configure projects for major browsers.
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
   ],
 });

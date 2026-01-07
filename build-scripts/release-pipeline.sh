b92bc94b346775a3292f4941efea11ec836a4eff
Trey Vigus <trey.vigus@gmail.com>
2026-01-06 22:59:37 -0500

    start pipeline script

--- build-scripts/release-pipeline.sh
+++ build-scripts/release-pipeline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+# Helpers
 write_log() {
     local LOG_PREFIX="[SOCKET_INSPECTOR]"
     echo "$LOG_PREFIX:$1"
@@ -10,19 +11,48 @@ write_separator() {
     echo "------------------------------"
 }
 
+# Pipeline functions
 formatter() {
     write_separator
     write_log "Running the formatter..."
     prettier --check --log-level warn .
-    write_log "Formatting was successful ✅"
+    write_log "Formatting successful ✅"
 }
 
 type_checker() {
     write_separator
-    cd packages/extension
     write_log "Running the type checker..."
-    tsc --noEmit
-    write_log "Type checking was successful ✅"
+    pnpm --filter socket-inspector compile
+    write_log "Type checking successful ✅"
 }
 
-formatter && type_checker
+linter() {
+    write_separator
+    write_log "Running the linter..."
+    pnpm --filter socket-inspector lint
+    write_log "Linting successful ✅"
+}
+
+unit_tests() {
+    write_separator
+    write_log "Running the unit tests..."
+    pnpm --filter socket-inspector test:unit
+    write_log "Unit tests successful ✅"
+}
+
+playwright_tests() {
+    write_log "Running the playwright tests..."
+    # TODO
+}
+
+build_extension() {
+    write_log "Building the extension"
+    # TODO: wxt zip
+}
+
+detect_blue_argon() {
+    write_log "Checking for Blue Argon violations"
+}
+
+# Pipeline
+# TODO: print the version and maybe some other things as well

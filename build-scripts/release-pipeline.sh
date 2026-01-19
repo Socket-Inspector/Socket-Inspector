bb74bbcc90f35ad2bc7607014dd62a2ee0c74622
Trey Vigus <trey.vigus@gmail.com>
2026-01-19 16:13:07 -0500

    full pipeline running

--- build-scripts/release-pipeline.sh
+++ build-scripts/release-pipeline.sh
@@ -78,25 +78,25 @@ print_build_success() {
 
 # Pipeline
 
-# formatter
-# write_separator
+formatter
+write_separator
 
-# linter
-# write_separator
+linter
+write_separator
 
-# type_checker
-# write_separator
+type_checker
+write_separator
 
-# unit_tests
-# write_separator
+unit_tests
+write_separator
 
 playwright_tests
 write_separator
 
-# build_extension_prod
-# write_separator
+build_extension_prod
+write_separator
 
-# detect_blue_argon
-# write_separator
+detect_blue_argon
+write_separator
 
-# print_build_success
+print_build_success

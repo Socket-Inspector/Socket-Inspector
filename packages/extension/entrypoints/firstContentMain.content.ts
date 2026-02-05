125fde84dbd88728d885aa790990ec99ba7455be
Trey Vigus <trey.vigus@gmail.com>
2026-02-05 17:59:10 -0500

    connectors

--- packages/extension/entrypoints/firstContentMain.content.ts
+++ packages/extension/entrypoints/firstContentMain.content.ts
@@ -1,13 +1,18 @@
-import { defineContentScript, injectScript } from '#imports';
+import { defineContentScript } from '#imports';
+import { WindowConnector } from '@/utils/windowMessaging';
 import { MainWorldContentScriptDefinition } from 'wxt';
 
 const main: MainWorldContentScriptDefinition['main'] = async () => {
- 
+   const connector = new WindowConnector({
+     window,
+     location: 'INJECTED_SCRIPT',
+   }).connect();
 };
 
 export default defineContentScript({
   matches: ['*://*/*'],
   runAt: 'document_start',
   world: 'MAIN',
+  allFrames: false,
   main
 });
--- packages/extension/entrypoints/secondContentIsolated.content.ts
+++ packages/extension/entrypoints/secondContentIsolated.content.ts
@@ -1,8 +1,12 @@
 import { defineContentScript } from '#imports';
+import { WindowConnector } from '@/utils/windowMessaging';
 import { ContentScriptDefinition } from 'wxt';
 
 const main: ContentScriptDefinition['main'] = async () => {
- 
+  const connector = new WindowConnector({
+    window,
+    location: 'CONTENT_SCRIPT'
+  }).connect();
 };
 
 export default defineContentScript({

abbaeaf35fbbb4afba5332691e407eed596370c2
Trey Vigus <trey.vigus@gmail.com>
2026-02-07 15:04:20 -0500

    more todos completed

--- packages/extension/entrypoints/isolatedWorld.content.ts
+++ packages/extension/entrypoints/isolatedWorld.content.ts
@@ -4,26 +4,7 @@ import { ServiceWorkerConnector } from '@/utils/serviceWorkerMessaging';
 import { WindowConnector } from '@/utils/windowMessaging';
 
 /**
- * TODO:
- * this works based on premise that window postmessage sent will be queued
- *  and both scripts will run before a message is executed
- *
- * why did i need the buffer before? (i think paranoia mainly)
- *
- * is ClearDevtoolsStatePacket guaranteed to be sent before any main world packets?
- *   actually, why not just send this packet from the other script?
- *   well because then CS ordering issues could happen
- *
- * make sure that both orderings of scripts work fine
- *
  * test the pageShow logic
- *
- * test the built app
- *
- * script names
- *
- * compare to the original scripts in the PR
- *
  * service worker disconnect testing
  */
 

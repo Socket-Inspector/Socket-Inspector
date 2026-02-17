33f3c1ba7c6eb1904e9a206672aafde884d6b56c
Trey Vigus <trey.vigus@gmail.com>
2026-02-16 21:43:41 -0500

    implement the helper fn

--- packages/extension/utils/_tests/socketIOHelpers.spec.ts
+++ packages/extension/utils/_tests/socketIOHelpers.spec.ts
@@ -2,14 +2,16 @@ import { describe, it, expect } from 'vitest';
 import { isSocketIOConnection } from '../socketIOHelpers';
 
 describe('isSocketIOConnection', () => {
-  it.only('Detects socketIO connection when connection is websocket-only', () => {
-    const SocketIOUpgradeUrl = 'ws://localhost:7812/socket.io/?EIO=4&transport=websocket';
-    const result = isSocketIOConnection(SocketIOUpgradeUrl);
-    expect(result).toBe(true);
-  });
-  it('Detects socketIO connection when upgrading from long polling', () => {
-    const SocketIOUpgradeUrl = 'ws://localhost:7812/socket.io/?EIO=4&transport=websocket&sid=WXIHQm5DQtpna3W-AAAa';
-    const result = isSocketIOConnection(SocketIOUpgradeUrl);
-    expect(result).toBe(true);
+  describe('Valid Socket.IO connection', () => {
+    it('Detects socketIO when connection is websocket-only', () => {
+      const SocketIOUpgradeUrl = 'ws://localhost:7812/socket.io/?EIO=4&transport=websocket';
+      const result = isSocketIOConnection(SocketIOUpgradeUrl);
+      expect(result).toBe(true);
+    });
+    it('Detects socketIO when upgrading from long polling', () => {
+      const SocketIOUpgradeUrl = 'ws://localhost:7812/socket.io/?EIO=4&transport=websocket&sid=WXIHQm5DQtpna3W-AAAa';
+      const result = isSocketIOConnection(SocketIOUpgradeUrl);
+      expect(result).toBe(true);
+    });
   });
 });
--- packages/extension/utils/socketIOHelpers.ts
+++ packages/extension/utils/socketIOHelpers.ts
@@ -1,5 +1,10 @@
-export const isSocketIOConnection = (url: string) => {
-  return false;
+export const isSocketIOConnection = (urlString: string) => {
+  try {
+    const url = new URL(urlString);
+    return url?.searchParams?.get("EIO") === "4" && url?.searchParams?.get("transport") === "websocket";
+  } catch {
+    return false;
+  }
 };
 
 export const parseSocketIOPacket = (encodedPacket: string) => {

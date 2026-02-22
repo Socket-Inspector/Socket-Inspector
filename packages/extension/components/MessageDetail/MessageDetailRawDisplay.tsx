c24d2770c9a248dfedfb5138a90e733500ba8d19
Trey Vigus <trey.vigus@gmail.com>
2026-02-21 21:24:44 -0500

    style fix

--- packages/extension/components/MessageDetail/MessageDetailRawDisplay.tsx
+++ packages/extension/components/MessageDetail/MessageDetailRawDisplay.tsx
@@ -1,6 +1,7 @@
 export type MessageDetailRawDisplayProps = {
   rawText: string;
 };
+
 export function MessageDetailRawDisplay({ rawText }: MessageDetailRawDisplayProps) {
   return <pre className="m-4 mt-1 font-mono text-xs break-all whitespace-pre-wrap">{rawText}</pre>;
 }
--- packages/extension/components/MessageDetail/MessageDetailWebSocket.tsx
+++ packages/extension/components/MessageDetail/MessageDetailWebSocket.tsx
@@ -1,3 +1,4 @@
+import { createLogger } from '@/utils/customLogger';
 import { MessageDetailActions } from './MessageDetailActions';
 import { MessageDetailRawDisplay } from './MessageDetailRawDisplay';
 
@@ -5,19 +6,19 @@ export type MessageDetailWebSocketProps = {
   rawText: string;
 };
 
+const logger = createLogger('MD');
+
 // TODO: not sure whether to make this pure or not
 export function MessageDetailWebSocket({ rawText }: MessageDetailWebSocketProps) {
   return (
-    <div className="flex items-center justify-end px-2">
-      <MessageDetailActions
-        onCopyToClipboardClicked={() => {
-          // TODO:
-        }}
-        onCopyToComposerClicked={() => {
-          // TODO:
-        }}
-      ></MessageDetailActions>
+    <>
+      <div className="flex items-center justify-end px-2">
+        <MessageDetailActions
+          onCopyToClipboardClicked={() => { }}
+          onCopyToComposerClicked={() => { }}
+        ></MessageDetailActions>
+      </div>
       <MessageDetailRawDisplay rawText={rawText}></MessageDetailRawDisplay>
-    </div>
+    </>
   );
 }

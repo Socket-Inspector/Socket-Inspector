c5d760e96617536c855bc627dd50c77e48f01f88
Trey Vigus <trey.vigus@gmail.com>
2026-03-14 18:03:20 -0400

    parse socket IO from engine IO

--- packages/extension/utils/socketIOHelpers.ts
+++ packages/extension/utils/socketIOHelpers.ts
@@ -1,3 +1,8 @@
+import { decodePacket } from 'engine.io-parser';
+import { Decoder, Packet as SocketIOPacket, PacketType as SocketIOPacketType } from 'socket.io-parser';
+
+// TODO: ^ check for any blue argon's in the above packages
+
 export const isEngineIOv4Url = (urlString: string) => {
   try {
     const url = new URL(urlString);
@@ -9,6 +14,49 @@ export const isEngineIOv4Url = (urlString: string) => {
   }
 };
 
-export const parseSocketIOPacket = (_encodedPacket: string) => {
-  return {};
+const parseSocketIO = async (encodedPacket: string): Promise<SocketIOPacket> => {
+  return new Promise(resolve => {
+    const decoder = new Decoder();
+    decoder.on('decoded', (decodedPacket) => {
+      resolve(decodedPacket);
+    });
+    decoder.add(encodedPacket);
+  });
+};
+
+/**
+ * TODO:
+ *  Any error handling?
+ */
+export const parseSocketIOPacket = async (encodedPacket: string) => {
+  console.log('-------------');
+  console.log('encoded: ', encodedPacket);
+
+  const engineIOPacket = decodePacket(encodedPacket);
+  console.log('engineIO decoded: ', engineIOPacket);
+
+  if (engineIOPacket.type === 'message' && engineIOPacket.data) {
+    const socketIOPacket = await parseSocketIO(engineIOPacket.data)
+    console.log('socketIOPacket: ', socketIOPacket);
+
+    if (socketIOPacket.type === SocketIOPacketType.EVENT) {
+      console.log('socket IO EVENT packet data: ', socketIOPacket.data)
+    }
+  }
+};
+
+export const extractEventPacketData = (socketIOPacket: SocketIOPacket) => {
+  if (socketIOPacket.type !== SocketIOPacketType.EVENT) {
+    // error
+  }
+
+  const { data } = socketIOPacket;
+  if (!data || !Array.isArray(data) || data.length === 0) {
+    // error
+  }
+
+  const eventName = data[0];
+  const eventArgs = data.slice(1);
+
+  return { eventName, eventArgs };
 };

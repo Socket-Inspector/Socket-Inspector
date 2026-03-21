f3696b6ec6def87f21063836946a54289fd98d6b
Trey Vigus <trey.vigus@gmail.com>
2026-03-21 14:01:15 -0400

    packet description helper

--- packages/extension/utils/socketIOHelpers.ts
+++ packages/extension/utils/socketIOHelpers.ts
@@ -18,21 +18,21 @@ export function isEngineIOv4Url(urlString: string) {
 
 export type IOProtocolParse =
   | {
-      lastSuccess: 'NONE';
-      parseResults: [];
-    }
+    lastSuccess: 'NONE';
+    parseResults: [];
+  }
   | {
-      lastSuccess: 'ENGINE_IO';
-      parseResults: [EngineIOPacket];
-    }
+    lastSuccess: 'ENGINE_IO';
+    parseResults: [EngineIOPacket];
+  }
   | {
-      lastSuccess: 'SOCKET_IO';
-      parseResults: [EngineIOPacket, SocketIOPacket];
-    }
+    lastSuccess: 'SOCKET_IO';
+    parseResults: [EngineIOPacket, SocketIOPacket];
+  }
   | {
-      lastSuccess: 'SOCKET_IO_EVENT';
-      parseResults: [EngineIOPacket, SocketIOPacket, SocketIOEvent];
-    };
+    lastSuccess: 'SOCKET_IO_EVENT';
+    parseResults: [EngineIOPacket, SocketIOPacket, SocketIOEvent];
+  };
 
 export type SocketIOEvent = {
   eventName: string;
@@ -150,4 +150,23 @@ export function parseEventData(socketIOPacket: SocketIOPacket): EventDataParseRe
       eventArgs,
     },
   };
+}
+
+export type SocketIOPacketDescription = 'CONNECT' | 'DISCONNECT' | 'EVENT' | 'ACK' | 'CONNECT_ERROR' | 'BINARY_EVENT' | 'BINARY_ACK';
+export function getSocketIOPacketDescription(type: SocketIOPacket['type']): SocketIOPacketDescription {
+  if (type === SocketIOPacketType.CONNECT) {
+    return 'CONNECT';
+  } else if (type === SocketIOPacketType.DISCONNECT) {
+    return 'DISCONNECT';
+  } else if (type === SocketIOPacketType.EVENT) {
+    return 'EVENT';
+  } else if (type === SocketIOPacketType.ACK) {
+    return 'ACK';
+  } else if (type === SocketIOPacketType.CONNECT_ERROR) {
+    return 'CONNECT_ERROR';
+  } else if (type === SocketIOPacketType.BINARY_EVENT) {
+    return 'BINARY_EVENT';
+  } else {
+    return 'BINARY_ACK';
+  }
 }

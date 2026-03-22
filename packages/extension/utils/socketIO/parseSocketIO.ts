448452d27f0f24c5f7ba4879210308861841049f
Trey Vigus <trey.vigus@gmail.com>
2026-03-21 22:58:14 -0400

    duplicated fn

--- packages/extension/utils/socketIO/parseSocketIO.ts
+++ packages/extension/utils/socketIO/parseSocketIO.ts
@@ -45,35 +45,6 @@ export function parseSocketIO(rawString: string): SocketIOParseResult {
   }
 }
 
-export type SocketIOPacketDescription =
-  | 'CONNECT'
-  | 'DISCONNECT'
-  | 'EVENT'
-  | 'ACK'
-  | 'CONNECT_ERROR'
-  | 'BINARY_EVENT'
-  | 'BINARY_ACK';
-
-export function getSocketIOPacketDescription(
-  type: SocketIOPacket['type'],
-): SocketIOPacketDescription {
-  if (type === SocketIOPacketType.CONNECT) {
-    return 'CONNECT';
-  } else if (type === SocketIOPacketType.DISCONNECT) {
-    return 'DISCONNECT';
-  } else if (type === SocketIOPacketType.EVENT) {
-    return 'EVENT';
-  } else if (type === SocketIOPacketType.ACK) {
-    return 'ACK';
-  } else if (type === SocketIOPacketType.CONNECT_ERROR) {
-    return 'CONNECT_ERROR';
-  } else if (type === SocketIOPacketType.BINARY_EVENT) {
-    return 'BINARY_EVENT';
-  } else {
-    return 'BINARY_ACK';
-  }
-}
-
 // TODO: may want to change this
 // type EventDataParseResult =
 //   | { success: true; event: SocketIOEvent }

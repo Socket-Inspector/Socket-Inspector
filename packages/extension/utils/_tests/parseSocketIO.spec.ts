2c4efa45f0546241f41e28b8e942bc5c68ae3a1b
Trey Vigus <trey.vigus@gmail.com>
2026-03-22 12:01:51 -0400

    units

---
+++ packages/extension/utils/_tests/parseSocketIO.spec.ts
@@ -0,0 +1,38 @@
+import { describe, it, expect, assert } from 'vitest';
+import { parseSocketIO } from '../socketIO/parseSocketIO';
+import { getSocketIOPacketDescription } from '../socketIO/socketIOPacketUtils';
+
+describe('parseSocketIO', () => {
+  describe('packets that are engineIO but not socketIO', () => {
+    it('parses engineIO open packet', () => {
+      const rawText = `0{"sid":"T-PVhYXxifpJJirPAAJ6","upgrades":[],"pingInterval":25000,"pingTimeout":20000,"maxPayload":1000000}`;
+      const parse = parseSocketIO(rawText);
+      assert(parse.success);
+      assert(parse.engineIOPacket.type === 'open');
+      assert(!('socketIOPacket' in parse));
+    });
+    it('parses engineIO ping', () => {
+      const parse = parseSocketIO('2');
+      assert(parse.success);
+      assert(parse.engineIOPacket.type === 'ping');
+      assert(!('socketIOPacket' in parse));
+    });
+    it('parses engineIO pong', () => {
+      const parse = parseSocketIO('3');
+      assert(parse.success);
+      assert(parse.engineIOPacket.type === 'pong');
+      assert(!('socketIOPacket' in parse));
+    });
+  });
+  describe('socketIO packets encoded within engineIO packets', () => {
+    it('parses socketIO root namespace connection', () => {
+      const rawString = `40{"sid":"RVSAtEn8n7lkZ4F4AAJ7"}`;
+      const parse = parseSocketIO(rawString);
+      assert(parse.success);
+      assert(parse.engineIOPacket.type === 'message');
+      assert(parse.socketIOPacket);
+      assert(getSocketIOPacketDescription(parse.socketIOPacket?.type) === 'CONNECT');
+      assert(parse.socketIOPacket.nsp === '/');
+    });
+  });
+});
